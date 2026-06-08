a7bae3113301a4a954fb00435d708a80ba16d87f
BastianFG <115721052+BastianFG@users.noreply.github.com>
2026-06-07 20:58:25 -0400

    feat: instalar etiqueta global de Google (gtag.js)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -98,8 +98,20 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 
 function RootShell({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18198577508" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18198577508');
+            `,
+          }}
+        />
         <HeadContent />
       </head>
       <body>
